c0449ce5d860ed63ea34f56961af6aaf2e7274a4
Jasmita Malik <Jasmita97@gmail.com>
2024-05-10 11:22:32 +0400

    Update imagenet_2012_labels.py

--- imagenet_stubs/imagenet_2012_labels.py
+++ imagenet_stubs/imagenet_2012_labels.py
@@ -1001,7 +1001,7 @@ IMAGENET_2012_LABELS = {
     997: 'bolete',
     998: 'ear, spike, capitulum',
     999: 'toilet tissue, toilet paper, bathroom tissue',
-    1000:'cat'
+    1000: 'cat'
 }
 
 IMAGENET_2012_LABELS_REVERSE = {v: k for k, v in
